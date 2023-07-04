b609c814ff13e68bdc634ec2d62e054f6402b9d3
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-07-04 06:26:54 +0300

    In memory datastore

---
+++ server/datastore/index.ts
@@ -0,0 +1,6 @@
+import { CommentDao } from "./CommentDao";
+import { LikeDao } from "./LikeDao";
+import { PostDao } from "./PostDao";
+import { UserDao } from "./UserDao";
+
+export interface Datastore extends UserDao, PostDao, LikeDao, CommentDao {}
---
+++ server/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("tiny"));
+
+app.listen(3000, () => {
+  console.log("Server runing on port 3000");
+});
