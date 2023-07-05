944d0c1fc8051a8357f74e3bcac2928e4dbcfa21
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-07-06 00:50:46 +0300

    dao refactor, add async handler

--- server/handlers/postHandler.ts
+++ server/handlers/postHandler.ts
@@ -4,21 +4,27 @@ import { ListPostsRequest, ListPostsResponse, createPostRequest, createPostRespo
 import { db } from '../datastore';
 import { ExpressHandler, Post } from '../types';
 
-export const listPostsHandler: ExpressHandler<ListPostsRequest, ListPostsResponse> = (
+export const listPostsHandler: ExpressHandler<ListPostsRequest, ListPostsResponse> = async (
   _req,
   res
 ) => {
-  res.send({ posts: db.listPosts() });
+  // TODO: add pagination and filtering
+  res.send({ posts: await db.listPosts() });
 };
 
 export const createPostHandler: ExpressHandler<createPostRequest, createPostResponse> = (
   req,
   res
 ) => {
+  // TODO: better error message
   if (!req.body.title || !req.body.url || !req.body.userId) {
     res.sendStatus(400);
     return;
   }
+  // TODO: validate user exists
+  // TODO: get user Id from session
+  // TODO: validate title and url are non-empty
+  // TODO: validate url is new, otherwise add +1 to existing post
   const post: Post = {
     id: crypto.randomUUID(),
     title: req.body.title,
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
 import morgan from 'morgan';
 import { createPostHandler, listPostsHandler } from './handlers/postHandler';
+import asyncHandler from 'express-async-handler';
 
 const app = express();
 
@@ -8,8 +9,15 @@ app.use(express.json());
 
 app.use(morgan('tiny'));
 
-app.get('/posts', listPostsHandler);
-app.post('/posts', createPostHandler);
+app.get('/v1/posts', asyncHandler(listPostsHandler));
+app.post('/v1/posts', asyncHandler(createPostHandler));
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  console.log('Uncaught exeception: ', err);
+  return res.status(500).send('Oops, an unexpected error occurred, please try again');
+};
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Server runing on port 3000');
